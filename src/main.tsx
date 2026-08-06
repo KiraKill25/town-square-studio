1b1c2774b0944d8f595247845335d7689d8785d9
KiraKill25 <45837225+KiraKill25@users.noreply.github.com>
2026-08-06 14:24:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { StatusBar } from "@capacitor/status-bar";
+import { SplashScreen } from "@capacitor/splash-screen";
+import { KeepAwake } from "@capacitor-community/keep-awake";
 import { getRouter } from "./router";
 import "./styles.css";
 
 // Hide the Android status bar for full-screen mode
 StatusBar.hide().catch(() => {});
+
+// Keep screen awake (prevent phone screen from turning off while app is running)
+KeepAwake.keepAwake().catch(() => {});
+
+// Hide the splash screen automatically after 2 seconds
+setTimeout(() => {
+  SplashScreen.hide().catch(() => {});
+}, 2000);
 
 const router = getRouter();
 
